25fd60b7fba80769ebb455dbf9795171b106c48e
Serg <s.ustin.92@mail.ru>
2023-11-27 17:09:23 +0300

    add delete file

--- src/main/java/com/example/telegrambot/service/command/Command.java
+++ src/main/java/com/example/telegrambot/service/command/Command.java
@@ -1,13 +1,5 @@
 package com.example.telegrambot.service.command;
 
-<<<<<<< HEAD
 public abstract class Command {
     public abstract boolean setCommand(String msg);
-=======
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Update;
-
-public interface Command {
-    SendMessage commandSelection();
->>>>>>> origin/master
 }
