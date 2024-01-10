88172cba73f3ddb4685e0f8ec7e179aa0090afd2
Serg <s.ustin.92@mail.ru>
2024-01-10 10:35:43 +0300

    fixed code

--- src/main/java/com/example/telegrambot/service/command/text/SendTextCommand.java
+++ src/main/java/com/example/telegrambot/service/command/text/SendTextCommand.java
@@ -1,6 +1,5 @@
 package com.example.telegrambot.service.command.text;
 
-import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
